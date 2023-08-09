965090f47e3b4c7a5d31bef35024b73ed037dfdb
Evgeniy Novikov <termius46@gmail.com>
2023-08-09 22:31:24 +0300

    fix: fix burger menu behaviour

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const menu = document.querySelector('.navigation__list');
 const burgerBtn = document.querySelector('.burger');
+const bg = document.querySelector('.bg');
+bg.classList.add('.bg');
 
 function handleShowBurger() {
   burgerBtn.classList.toggle('burger--active');
   menu.classList.toggle('navigation__list--active');
-  // document.body.classList.toggle('lock');
-  // shadow.classList.toggle('shadow--active');
+  document.body.classList.toggle('lock');
+  bg.classList.toggle('bg--active');
 }
 
-// shadow.addEventListener('click', () => {
-//   handleShowBurger();
-// });
+bg.addEventListener('click', () => {
+  handleShowBurger();
+});
 
 burgerBtn.addEventListener('click', () => {
   handleShowBurger();
@@ -18,7 +20,7 @@ burgerBtn.addEventListener('click', () => {
 
 menu.addEventListener('click', (e) => {
   if (window.innerWidth <= 930) {
-    if (e.target.classList.contains('header__link')) {
+    if (e.target.classList.contains('navigation__link')) {
       setTimeout(() => {
         handleShowBurger();
       }, 100);
