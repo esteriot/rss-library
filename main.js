07958685137f286f859fdad3b298f2c26c2fccc3
Evgeniy Novikov <termius46@gmail.com>
2023-09-07 12:05:52 +0300

    feat: add card checking

--- main.js
+++ main.js
@@ -7,9 +7,16 @@ const profileBtn = document.querySelector('.header__profile-icon');
 const modal = document.querySelector('.modal');
 const profile = document.querySelector('.modal-profile');
 const logOutBtn = document.querySelector('.modal-profile__logout');
+const cardGet = document.querySelector('.card__get');
+const cardProfile = document.querySelector('.card__profile');
+const cardHeading = document.querySelector('.card__check-heading');
 const regForm = document.forms.register;
 const loginForm = document.forms.login;
 const { cardCheckForm } = document.forms;
+const checkCardBtn = document.querySelector('.button--form');
+const cardInfo = document.querySelector('.card-info');
+const formUsername = document.querySelector('.form__username');
+const formCardNum = document.querySelector('.form__card-number');
 
 let userFirstName;
 let userLastName;
@@ -32,9 +39,34 @@ function validatePassword(password) {
   return testResult;
 }
 
+function showCardInfo() {
+  checkCardBtn.style.display = 'none';
+  cardInfo.style.display = 'flex';
+  formUsername.style.color = '#bb945f';
+  const userName = `${localStorage.getItem('firstName')} ${localStorage.getItem(
+    'lastName'
+  )}`;
+  formUsername.value = userName;
+  formUsername.setAttribute('disabled', 'disabled');
+  formCardNum.style.color = '#bb945f';
+  formCardNum.value = localStorage.getItem('cardNumber');
+  formCardNum.setAttribute('disabled', 'disabled');
+  checkCardBtn.setAttribute('disabled', 'disabled');
+}
+
+function hideCardInfo() {
+  checkCardBtn.style.display = 'block';
+  cardInfo.style.display = 'none';
+  formUsername.style.color = '#8e8e8e';
+  formUsername.removeAttribute('disabled', 'disabled');
+  formCardNum.style.color = '#8e8e8e';
+  formCardNum.removeAttribute('disabled', 'disabled');
+  cardCheckForm.reset();
+  checkCardBtn.removeAttribute('disabled', 'disabled');
+}
+
 function isLogged() {
   isLoggedIn = JSON.parse(localStorage.getItem('isLoggedIn'));
-
   if (isLoggedIn) {
     userFirstName = localStorage.getItem('firstName');
     userLastName = localStorage.getItem('lastName');
@@ -44,6 +76,10 @@ function isLogged() {
     profile.children[4].style.display = 'inline-block';
     profileBtn.textContent = userFirstName.at(0) + userLastName.at(0);
     profileBtn.classList.add('header__profile-icon--logged');
+    cardGet.style.display = 'none';
+    cardProfile.style.display = 'flex';
+    cardHeading.textContent = 'Your Library card';
+    showCardInfo();
   } else {
     profile.children[1].style.display = 'inline-block';
     profile.children[2].style.display = 'inline-block';
@@ -56,6 +92,10 @@ function isLogged() {
                 d="M28 14C28 21.732 21.732 28 14 28C6.26801 28 0 21.732 0 14C0 6.26801 6.26801 0 14 0C21.732 0 28 6.26801 28 14ZM18.6667 7.77778C18.6667 10.3551 16.5774 12.4444 14.0001 12.4444C11.4227 12.4444 9.33339 10.3551 9.33339 7.77778C9.33339 5.20045 11.4227 3.11111 14.0001 3.11111C16.5774 3.11111 18.6667 5.20045 18.6667 7.77778ZM19.4998 16.2781C20.9584 17.7367 21.7778 19.715 21.7778 21.7778H14L6.22225 21.7778C6.22225 19.715 7.0417 17.7367 8.50031 16.2781C9.95893 14.8194 11.9372 14 14 14C16.0628 14 18.0411 14.8194 19.4998 16.2781Z"
                 fill="white" />
             </svg>`;
+    cardGet.style.display = 'flex';
+    cardProfile.style.display = 'none';
+    cardHeading.textContent = 'Find your Library card';
+    hideCardInfo();
   }
 }
 
@@ -116,32 +156,21 @@ logOutBtn.addEventListener('click', () => {
   isLogged();
 });
 
-// card check
-
-const checkCardBtn = document.querySelector('.button--form');
-const cardInfo = document.querySelector('.card-info');
-
 cardCheckForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const fullName = `${userFirstName} ${userLastName}`;
+  const fullName = `${localStorage.getItem('firstName')} ${localStorage.getItem(
+    'lastName'
+  )}`;
   const cardNumber = localStorage.getItem('cardNumber');
-  const userName = cardCheckForm.elements.name;
-  const cardNum = cardCheckForm.elements.number;
 
-  if (fullName === userName.value && cardNumber === cardNum.value) {
-    checkCardBtn.style.display = 'none';
-    cardInfo.style.display = 'flex';
-    userName.style.color = '#bb945f';
-    cardNum.style.color = '#bb945f';
+  if (fullName === formUsername.value && cardNumber === formCardNum.value) {
+    showCardInfo();
     setTimeout(() => {
-      checkCardBtn.style.display = 'block';
-      cardInfo.style.display = 'none';
-      userName.style.color = '#8e8e8e';
-      cardNum.style.color = '#8e8e8e';
-      cardCheckForm.reset();
-    }, 5000);
+      hideCardInfo();
+    }, 3000);
   } else {
     alert('You card is not valid');
+    cardCheckForm.reset();
   }
 });
 
